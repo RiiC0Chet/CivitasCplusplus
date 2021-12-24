7b7b2580a8fcad4b678c083373bf6caa82671a0a
Jose Luis Rico Ramos <jlricoramos@correo.ugr.es>
2021-12-24 18:21:15 +0100

    Agregar archivos de proyecto.

---
+++ Casilla.h
@@ -0,0 +1,21 @@
+#pragma once
+#include <string>
+
+using namespace std;
+
+class Casilla
+{
+private:
+	string nombre;
+
+public:
+
+	inline string getNombre()
+	{
+		return nombre;
+	}
+		
+	
+
+};
+
