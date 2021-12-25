44ea9fcb83e66f39d8428585a181c71099735794
Jose Luis Rico Ramos <jlricoramos@correo.ugr.es>
2021-12-25 14:39:05 +0100

    Terminando CasillaCalle

--- CasillaCalle.cpp
+++ CasillaCalle.cpp
@@ -48,8 +48,62 @@ bool civitas::CasillaCalle::esEsteElPropietario(Jugador &jugador)
     else return false;
 }
 
-void recibeJugador(int i_actual, vector<Jugador> todos);
-bool tienePropietario();
-void tramitarAlquiler(Jugador& jugador);
-bool igualdadEstado(CasillaCalle &otraCasilla);
-string toString();
+void civitas::CasillaCalle::recibeJugador(int i_actual, vector<Jugador> todos)
+{
+    informe(i_actual, todos);
+    Jugador aux = todos.at(i_actual);
+
+
+    if (!this->tienePropietario()) {
+        aux.puedeComprarCasilla();
+    }
+    else {
+        tramitarAlquiler(aux);
+    }
+}
+bool civitas::CasillaCalle::tienePropietario()
+{
+    if (propietario != nullptr)
+        return true;
+    else return false;
+}
+void civitas::CasillaCalle::tramitarAlquiler(Jugador& jugador)
+{
+    if (!esEsteElPropietario(jugador) && this->tienePropietario()) {
+
+        float precio = this->getPrecioAlquilerCompleto();
+        std::cout<<"El propietario de la propiedad es: " << this->propietario.getNombre() << " y " << jugador.getNombre() << " tiene que pagar: " << precio << " a no ser que sea especulador."<<endl;
+
+
+        float nuevo_precio = jugador.paga(precio);
+        this->propietario.modificarSaldo(nuevo_precio * (-1));
+
+    }
+}
+bool civitas::CasillaCalle::igualdadEstado(CasillaCalle& otraCasilla)
+{
+    if (this == (&otraCasilla)) 
+    {
+        return true;
+    }
+    else 
+    {
+
+
+        if (this->getNombre().compare(otraCasilla.getNombre()) && precioCompra == otraCasilla.getPrecioCompra()
+            && precioEdificar == otraCasilla.getPrecioEdificar() && this->getPrecioAlquilerCompleto() == otraCasilla.getPrecioAlquilerCompleto()
+            && numCasas == otraCasilla.getNumCasas() && numHoteles == getNumHoteles()) 
+        {
+            return true;
+        }
+
+        else return false;
+    }
+}
+string civitas::CasillaCalle::toString():Casilla() // Ta mal
+{
+    if (this->propietario == nullptr)
+        return (". Precios: Compra: " + precioCompra + ", Edificar: " + precioEdificar + ", Alquiler base: " + precioBaseAlquiler + ", Alquiler Completo: " + getPrecioAlquilerCompleto() + ", Casas: " + numCasas + ", Hoteles: " + numHoteles);
+    else return  (" con el propietario " + propietario.getNombre());
+
+}
