dac2ed8135acf1b13d3005313fae5257bbfc2cc9
Jose Luis Rico Ramos <jlricoramos@correo.ugr.es>
2021-12-25 13:43:09 +0100

    CasillaCalle cosas

--- CasillaCalle.cpp
+++ CasillaCalle.cpp
@@ -2,5 +2,54 @@
 
 civitas::CasillaCalle::CasillaCalle(string n, int precio_Compra, int precio_Edificar, int precio_Base_alquiler):Casilla(n)
 {
-
+	this->propietario = nullptr;
+    this->precioCompra = precio_Compra;
+    this->precioEdificar = precio_Edificar;
+    this->precioBaseAlquiler = precio_Base_alquiler;
+    this->numCasas = 0;
+    this->numHoteles = 0;
 }
+
+bool civitas::CasillaCalle::comprar(Jugador jugador)
+{
+    (&propietario) = (&jugador);
+    jugador.paga(getPrecioCompra());
+    return true;
+}
+
+bool civitas::CasillaCalle::construirCasa(Jugador jugador)
+{
+    jugador.paga(precioEdificar);
+    numCasas++;
+    return true;
+}
+
+bool civitas::CasillaCalle::construirHotel(Jugador jugador)
+{
+    this->propietario.paga(precioEdificar);
+    numHoteles++;
+    return true;
+}
+
+bool civitas::CasillaCalle::derruirCasas(int numero, Jugador jugador)
+{
+    if (this->esEsteElPropietario(jugador) && numCasas >= numero) 
+    {
+        numCasas = numCasas - numero;
+        return true;
+    }
+    else return false;
+}
+
+bool civitas::CasillaCalle::esEsteElPropietario(Jugador jugador)
+{
+    if ((&jugador) == (&propietario))
+        return true;
+    else return false;
+}
+
+void recibeJugador(int i_actual, vector<Jugador> todos);
+bool tienePropietario();
+void tramitarAlquiler(Jugador jugador);
+bool igualdadEstado(CasillaCalle otraCasilla);
+string toString();
--- CasillaCalle.h
+++ CasillaCalle.h
@@ -15,6 +15,53 @@ namespace civitas
 			Jugador propietario;
 		public:
 			CasillaCalle(string n, int precio_Compra, int precio_Edificar, int precio_Base_alquiler);
+			bool comprar(Jugador jugador);
+			bool construirCasa(Jugador jugador);
+			bool construirHotel(Jugador jugador);
+			bool derruirCasas(int numero, Jugador jugador);
+			bool esEsteElPropietario(Jugador jugador);
+
+			inline int cantidadCasasHoteles()
+			{
+				return (numCasas + numHoteles);
+			}
+			
+			inline float getPrecioCompra()
+			{
+				return precioCompra;
+			}
+
+			inline float getPrecioEdificar() 
+			{
+				return precioEdificar;
+			}
+
+			inline int getNumCasas() 
+			{
+				return numCasas;
+			}
+
+			inline int getNumHoteles() 
+			{
+				return numHoteles;
+			}
+
+			inline float getPrecioAlquilerCompleto() 
+			{
+				return (precioBaseAlquiler * (FACTORALQUILERCASA + numCasas + numHoteles * FACTORALQUILERHOTEL));
+			}
+
+			void recibeJugador(int i_actual, vector<Jugador> todos);
+			bool tienePropietario();
+			void tramitarAlquiler(Jugador jugador);
+			bool igualdadEstado(CasillaCalle otraCasilla);
+
+			inline void actualizaPropietarioPorConversion(Jugador nuevo)
+			{
+				this->propietario = nuevo;
+			}
+
+			string toString();
 	};
 
 }
